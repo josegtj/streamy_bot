cc2c2baaf29a65869f2e75d472102b62a56e0aa1
josegtj <jose.gtjr@gmail.com>
2024-06-19 16:21:00 -0400

    Atualizando para usar a nova função

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from twitchio.ext import commands, routines
+from cryptography.fernet import Fernet
 import twitchio.errors
 import os
 import auth
 import json
 import model
-from cryptography.fernet import Fernet
+import time
 import asyncio
+
 
 key = os.getenv("ENCRYPT_KEY")
 cipher_suite = Fernet(key)
@@ -17,7 +19,9 @@ token_dict = json.loads(decrypted_data.decode())
 
 token = token_dict["access_token"]
 PREFIX = "!"
-CHANNEL = ["jose_gtj"]
+CHANNEL = ["nenanee_"]
+
+chat = model.create_chat()
 
 #Configurar o bot
 class Bot(commands.Bot):
@@ -33,24 +37,18 @@ class Bot(commands.Bot):
         print(f'Logado como: {self.nick}')
         print(f'O id de usuário é: {self.user_id}')
 
-    @routines.routine(seconds=60)
+    @routines.routine(seconds=60, wait_first=True)
     async def check_stream_status():
         stream = await bot.fetch_streams(user_logins = CHANNEL)
-        if stream:
+        if stream == []:
             print("Nena está on")
             bot.isLive += 1
             if bot.isLive == 1:
-                chan = await bot.get_channel(CHANNEL[0])
+                chan = bot.get_channel(CHANNEL[0])
                 if chan:
-                    response = chat.send_message("A Nena acabou de começar a live! Dê um olá ao pessoal do chat")
-                    try:
-                        await chan.send(response.text)
-                        print(f"Bot diz: {response.text}")
-                    except genai.types.StopCandidateException as e:
-                        response = chat.send_message("Você quase falou besteira! Peça desculpas imediatamente.")
-                        await chan.send(response.text)
-                        print(f"Failed to send message due to safety concerns: {e}")
-                        print(f"Safety ratings: {e.safety_ratings}")
+                    chat = model.create_chat()
+                    response = model.send_message("A Nena acabou de começar uma live! Dê um oi ao pessoal do chat!", chat=chat)
+                    await chan.send(response.text)
         else:
             print("Nena está off")
             bot.isLive == 0
@@ -58,33 +56,29 @@ class Bot(commands.Bot):
     @commands.command()
     async def ask(self, ctx: commands.Context, *, message: str,) -> None:
         # Comando !ask, que será chamado quando algum usuário utilizá-lo
-        if ctx.author.name == bot.user:
-            # Verificando se o usuário é o mesmo da mensagem anterior
-            print(f"{bot.user} (O mesmo) disse: {message}")
-            response = chat.send_message(f"O mesmo usário diz: {message}")
-            print(f"Bot disse: {response.text}")
-        else:
-            # Caso contrário, inicia um novo chat com o usuário
-            bot.user = ctx.author.name
-            model.start_chat(history=[])
-            print(f"{bot.user} disse: {message}")
-            response = chat.send_message(f"{ctx.author.name} diz: {message}")
-            print(f"Bot disse: {response.text}")
+        global chat
+        print(f"{ctx.author} diz: {message}")
+        response = model.send_message(message,chat,ctx.author)
         await ctx.send(response.text)
 
     check_stream_status.start()
 
-bot = Bot()
-try:
-    # Inicia o bot
-    bot.run()
-except twitchio.errors.AuthenticationError as e:
-    # Erro quando os tokens expiram
-    auth.refresh()
-    token = os.getenv("ACCESS_TOKEN")
+max_retries = 3
+retry_delay = 1
+for attempt in range(1, max_retries + 1):
     try:
+        # Inicia o bot
+        print("Bot iniciando...")
+        bot = Bot()
         bot.run()
-    except:
-        print("Falha grave no sistema!")
-    print("Falha na autenticação, tentando novo token")
-    print(e)
+    except twitchio.errors.AuthenticationError as e:
+        # Erro quando os tokens expiram
+        if attempt == max_retries:
+            print("Número máximo de tentativas alcançado")
+            break
+        else:
+            asyncio.run(auth.refresh())
+            token = os.getenv("ACCESS_TOKEN")
+            time.sleep(retry_delay)
+            print("Falha na autenticação, tentando novo token")
+            print(e)
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import google.generativeai as genai
+import google.api_core.exceptions
 import os
+import time
 
 #Criar o modelo de IA
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -9,21 +11,33 @@ context = open("file_context.txt", "r", encoding="UTF-8")
 model = genai.GenerativeModel('gemini-1.5-flash', system_instruction=context, 
                               safety_settings= {genai.types.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT:
                                                 genai.types.HarmBlockThreshold.BLOCK_ONLY_HIGH})
-#Criando um novo chat com a IA
-def create_chat(history:list=[]):
-    chat = model.start_chat(history=history)
+
+def create_chat():
+    chat = model.start_chat(history=[])
     return chat
 
-def send_message(message:str, author:str=""):
-    chat = create_chat()
-    response = chat.send_message(f"{author} diz: {message}")
-    if message._done == True:
-        return response
-    else:
-        
+#Criando o chat e mandando a mensagem
+def send_message(message:str, chat, author:str=""):
+    max_retries = 3
+    retry_delay = 1
+    for attempt in range(1, max_retries + 1):
+        try:
+            response = chat.send_message(f"{author} diz: {message}")
+        except google.api_core.exceptions.InternalServerError as e:
+            if attempt == max_retries:
+                print("Número máximo de tentativas alcançado")
+                response = {"text":"Peço perdão pelo transtorno, mas parece que o meu cérebro deu uma fritada! Tente novamente daqui a pouco."}
+                return response
+            else:
+                print(e)
+                print(f"Erro interno do sistema Google, tentando novamente em {retry_delay} segundos")
+                time.sleep(retry_delay)
+        except genai.types.StopCandidateException as e:
+            print(e)
+            print("Erro nos filtros de segurança, reformulando mensagem.")
+            response = chat.send_message(f"{author} quase fez você dizer bobagem! Dê um sermão EDUCADO nele")
+            return response
+        else:
+            print(f"O bot diz: {response.text}")
+            return response
 
-message = send_message("Olá bot")
-if message._done == True:
-    print(message.text)
-else:
-    print("Erro no envio da mensagem")
